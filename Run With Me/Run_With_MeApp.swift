bc03596c89a49f6332e0ef5d83769f232decb31a
Allen Jue <mrallenjue@yahoo.com>
2022-11-17 18:58:09 -0600

    Initial Commit

---
+++ Run With Me/Run_With_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_With_MeApp.swift
+//  Run With Me
+//
+//  Created by Allen Jue on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Run_With_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
